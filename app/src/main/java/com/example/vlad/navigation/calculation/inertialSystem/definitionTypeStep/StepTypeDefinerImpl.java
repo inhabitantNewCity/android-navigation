b5436e36ed095006f261906829b30fa0ee8b1831
inhabitantNewCity <v.andreev.job@gmail.com>
2019-01-07 13:00:18 +0300

    implementation algorithm

--- app/src/main/java/com/example/vlad/navigation/calculation/inertialSystem/definitionTypeStep/StepTypeDefinerImpl.java
+++ app/src/main/java/com/example/vlad/navigation/calculation/inertialSystem/definitionTypeStep/StepTypeDefinerImpl.java
@@ -2,9 +2,27 @@ package com.example.vlad.navigation.calculation.inertialSystem.definitionTypeSte
 
 import com.example.vlad.navigation.utils.StepType;
 
+import math.geom3d.Vector3D;
+
+
 public class StepTypeDefinerImpl implements StepTypeDefiner {
+
+    private static final double ANGLE_BOUND_STEP_TYPE = Math.sqrt(2)/2;
+
     @Override
     public StepType define(float[] ac) {
-        return null;
+        Vector3D vector = new Vector3D(ac[0],ac[1],ac[2]);
+        Vector3D vectorZ = new Vector3D(0,0,1);
+        vector.normalize();
+
+        double cosAngle = getAngleBetweenVectors(vector, vectorZ);
+        if(cosAngle >= ANGLE_BOUND_STEP_TYPE)
+            return StepType.STAIR;
+        return StepType.WALK;
+    }
+
+    private double getAngleBetweenVectors(Vector3D vector, Vector3D vectorZ) {
+        double multipleResult = vector.getX() * vectorZ.getX() + vector.getY() * vectorZ.getY() + vector.getZ() * vectorZ.getZ();
+        return Math.abs(multipleResult/(vector.norm() * vectorZ.norm()));
     }
 }
