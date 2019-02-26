5878bc750129d87820262b139948662d464eff0d
inhabitantNewCity <v.andreev.job@gmail.com>
2019-02-26 16:57:42 +0300

    add dynamic properties for configuration stub

--- app/src/main/java/com/example/vlad/navigation/database/DataAccessFactory.java
+++ app/src/main/java/com/example/vlad/navigation/database/DataAccessFactory.java
@@ -1,8 +1,17 @@
 package com.example.vlad.navigation.database;
 
+import com.example.vlad.navigation.utils.DynamicPropertyResolver;
+
 public class DataAccessFactory {
+    private static DataAccessService instance;
+    private static final String STUB_IS_ENABLED = "db.enable";
 
     public static DataAccessService getDataAccess(){
-        return new DataAccessServiceStub();
+        if(instance == null){
+            instance = DynamicPropertyResolver.getBooleanProperty(STUB_IS_ENABLED, true)
+                    ? new DataAccessServiceStub()
+                    : new DataAccessServiceRest();
+        }
+        return instance;
     }
 }
---
+++ app/src/main/java/com/example/vlad/navigation/utils/DynamicPropertyResolver.java
@@ -0,0 +1,39 @@
+package com.example.vlad.navigation.utils;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class DynamicPropertyResolver {
+//values/colors.xml
+    private static final String CONFIGURATION_FILE_NAME = "application/application.properties";
+
+    public static String getStringProperty(String key, String defaultValue) {
+        try {
+            Properties properties = new Properties();
+            InputStream inputStream = DynamicPropertyResolver.class.getResourceAsStream(CONFIGURATION_FILE_NAME);
+            properties.load(inputStream);
+            String value = properties.getProperty(key);
+            return value != null ? value : defaultValue;
+        } catch (IOException e){
+            Log.e("DynamicPropertyResolver", e.getMessage());
+        }
+        return defaultValue;
+
+    }
+
+    public static Boolean getBooleanProperty(String key, Boolean defaultValue){
+        try {
+            Properties properties = new Properties();
+            InputStream inputStream = DynamicPropertyResolver.class.getResourceAsStream(CONFIGURATION_FILE_NAME);
+            properties.load(inputStream);
+            Boolean value = new Boolean(properties.getProperty(key));
+            return value != null ? value : defaultValue;
+        } catch (IOException e){
+            Log.e("DynamicPropertyResolver", e.getMessage());
+        }
+        return  new Boolean(defaultValue);
+    }
+}
